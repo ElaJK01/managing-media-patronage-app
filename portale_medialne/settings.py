0c8b5dc1d0f52af25bcff7270d27bc640b649a94
Elżbieta Janus-Kaleta <60756867+ElaJK01@users.noreply.github.com>
2021-02-13 18:04:36 +0100

    added django_heroku

--- portale_medialne/settings.py
+++ portale_medialne/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 from decouple import config, Csv
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -163,3 +164,5 @@ EMAIL_USE_TLS = True
 
 LOGIN_REDIRECT_URL = 'event_list'
 LOGOUT_REDIRECT_URL = 'event_list'
+
+django_heroku.settings(locals())
